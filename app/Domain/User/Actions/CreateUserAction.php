ffbfc0cfb3b0fa6ce1e0a9d23c0373452294f1d8
jkryszto00 <jakubkryszto@gmail.com>
2022-11-07 14:17:42 +0100

    create user action

--- app/Domain/User/Actions/CreateUserAction.php
+++ app/Domain/User/Actions/CreateUserAction.php
@@ -3,16 +3,29 @@
 namespace App\Domain\User\Actions;
 
 use App\Domain\User\Data\UserData;
+use App\Domain\User\Mail\UserCreated;
 use App\Domain\User\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class CreateUserAction
 {
     public function __invoke(UserData $userData): User
     {
-        return User::create([
+        $user = User::create([
             'name' => $userData->name,
             'email' => $userData->email,
             'password' => $userData->password
         ]);
+
+        if ($user) {
+            $this->sendMail($user);
+        }
+
+        return $user;
+    }
+
+    private function sendMail(User $user): void
+    {
+        Mail::to($user->email)->send(new UserCreated($user->name));
     }
 }
