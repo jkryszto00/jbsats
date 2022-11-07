8516289ff2ec9df46f6a00bec118e39dcf9e82ce
jkryszto00 <jakubkryszto@gmail.com>
2022-11-07 14:10:22 +0100

    user created mail class

---
+++ app/Domain/User/Mail/UserCreated.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Domain\User\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected string $name
+    ){}
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('noreply@jbsats.com', 'jbsats.com'),
+            subject: 'Account created successfully'
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            text: 'emails.user.created',
+            with: [
+                'name' => $this->name
+            ]
+        );
+    }
+}
