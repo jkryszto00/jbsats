cbaacce2fced9c84a0095262150b2da57e0d2184
jkryszto00 <jakubkryszto@gmail.com>
2022-11-10 15:58:25 +0100

    added job offer category to controller

--- app/Http/Controllers/Panel/JobOffer/StoreJobOfferController.php
+++ app/Http/Controllers/Panel/JobOffer/StoreJobOfferController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Panel\JobOffer;
 
 use App\Domain\JobOffer\Actions\CreateJobOfferAction;
+use App\Domain\JobOffer\Data\CategoryData;
 use App\Domain\JobOffer\Data\Contract\ContractData;
 use App\Domain\JobOffer\Data\Contract\SalaryData;
 use App\Domain\JobOffer\Data\JobOfferData;
 use App\Domain\JobOffer\Enums\JobOfferLevel;
+use App\Domain\JobOffer\Models\Category;
 use App\Domain\JobOffer\Requests\CreateJobOfferRequest;
 
 class StoreJobOfferController
@@ -14,6 +16,8 @@ class StoreJobOfferController
     public function __invoke(CreateJobOfferRequest $request, CreateJobOfferAction $createJobOfferAction)
     {
         $validated = $request->validated();
+
+        $categories = Category::whereIn('id', $validated['category'])->get();
         $salaries = [];
 
         foreach ($validated['salary'] as $salary) {
@@ -24,6 +28,7 @@ class StoreJobOfferController
         $jobOffer = JobOfferData::from([
             'title' => $validated['title'],
             'description' => $validated['description'],
+            'category' => CategoryData::collection($categories),
             'level' => JobOfferLevel::from($validated['level']),
             'contract' => ContractData::from($validated['contract']),
             'salary' => SalaryData::collection($salaries)
