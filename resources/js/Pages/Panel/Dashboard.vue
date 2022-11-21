a0c6288ae5d6f13914d249f76ddf82be3ecce7f5
jkryszto00 <jakubkryszto@gmail.com>
2022-11-21 22:53:31 +0100

    dashboard new design

--- resources/js/Pages/Panel/Dashboard.vue
+++ resources/js/Pages/Panel/Dashboard.vue
@@ -1,50 +1,56 @@
 <script setup>
-import PanelLayout from "@/Layouts/PanelLayout.vue";
 import { Link } from '@inertiajs/inertia-vue3';
+
+import PanelLayout from "@/Layouts/PanelLayout.vue";
+import Card from "@/Components/Card.vue"
+import Button from "@/Components/Button.vue"
 
 defineProps(['counts', 'applications'])
 </script>
-
 <template>
     <PanelLayout>
-        <div class="flex justify-between gap-4">
-            <div class="w-2/3 p-4 bg-neutral-100">
-                <div class="pb-4 border-b border-neutral-200 flex items-center justify-between">
-                    <span class="font-bold text-xl">Job offers</span>
-                    <Link :href="route('panel.posting.index')" class="bg-neutral-900 text-neutral-50 px-4 py-2">Go to offers</Link>
-                </div>
-                <div class="pt-4 flex justify-between gap-4">
-                    <div class="flex-1 p-10 border border-neutral-200 bg-green-100 text-center">
-                        <span class="block text-lg">{{ counts.published }}</span>
-                        <Link :href="route('panel.posting.index', { status: 'published' })" class="text-green-900 hover:underline">Published</Link>
+        <div class="flex justify-between items-start gap-4">
+            <Card class="w-2/3">
+                <template #header>
+                    <div class="flex items-center justify-between">
+                        <span class="font-bold text-lg text-neutral-900">Job offers</span>
+                        <Link :href="route('panel.posting.index')"><Button>Go to offers</Button></Link>
                     </div>
-                    <div class="flex-1 p-10 border border-neutral-200 bg-red-100 text-center">
-                        <span class="block text-lg">{{ counts.expired }}</span>
-                        <Link :href="route('panel.posting.index', { status: 'expired' })" class="text-red-900 hover:underline">Expired</Link>
+                </template>
+                <div class="flex items-center divide-x divide-neutral-200">
+                    <div class="flex-1 text-center py-4">
+                        <Link :href="route('panel.posting.index', { status: 'published' })" class="text-lg font-semibold text-neutral-700 hover:underline">Published</Link>
+                        <div class="text-2xl font-bold text-green-600">{{ counts.published }}</div>
                     </div>
-                    <div class="flex-1 p-10 border border-neutral-200 bg-gray-200 text-center">
-                        <span class="block text-lg">{{ counts.draft }}</span>
-                        <Link :href="route('panel.posting.index', { status: 'drafts' })" class="text-gray-900 hover:underline">Drafts</Link>
+                    <div class="flex-1 text-center py-4">
+                        <Link :href="route('panel.posting.index', { status: 'expired' })" class="text-lg font-semibold text-neutral-700 hover:underline">Expired</Link>
+                        <div class="text-2xl font-bold text-red-600">{{ counts.expired }}</div>
+                    </div>
+                    <div class="flex-1 text-center py-4">
+                        <Link :href="route('panel.posting.index', { status: 'drafts' })" class="text-lg font-semibold text-neutral-700 hover:underline">Drafts</Link>
+                        <div class="text-2xl font-bold text-indigo-600">{{ counts.draft }}</div>
                     </div>
                 </div>
-            </div>
-            <div class="w-1/3 p-4 bg-neutral-100">
-                <div class="pb-4 border-b border-neutral-200 flex items-center justify-between">
-                    <span class="font-bold text-xl">Applications</span>
-                    <Link class="bg-neutral-900 text-neutral-50 px-4 py-2">Go to applications</Link>
-                </div>
-                <div class="pt-4 divide-y divide-neutral-200">
-                    <template v-for="application in applications">
-                        <div class="py-2 flex justify-between items-center">
+            </Card>
+            <Card class="w-1/3">
+                <template #header>
+                    <div class="flex items-center justify-between">
+                        <span class="font-bold text-lg text-neutral-900">Applications</span>
+                        <Link :href="route('panel.apply.index')"><Button>Go to applications</Button></Link>
+                    </div>
+                </template>
+                <div class="divide-y divide-neutral-200">
+                    <template v-for="apply in applications">
+                        <div class="py-2 first:pt-0 last:pb-0 flex justify-between items-center">
                             <div>
-                                <div>{{ application.candidate.name }}</div>
-                                <span class="text-sm">{{ application.job_offer.title }}</span>
+                                <div class="font-semibold text-neutral-700">{{ apply.candidate.name }}</div>
+                                <div class="text-sm text-neutral-500">{{ apply.job_offer.title }}</div>
                             </div>
-                            <span class="px-2 py-1 capitalize text-sm rounded-full bg-neutral-200 text-neutral-600">{{ application.created_at }}</span>
+                            <span class="px-2 py-0.5 rounded-full text-sm text-neutral-500 border border-neutral-200"> 2 days ago</span>
                         </div>
                     </template>
                 </div>
-            </div>
+            </Card>
         </div>
     </PanelLayout>
 </template>
