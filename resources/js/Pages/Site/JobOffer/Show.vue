c191065552a5c9034259fb82b9e6625096df8c53
jkryszto00 <jakubkryszto@gmail.com>
2022-11-23 18:48:09 +0100

    job offer list and show view

--- resources/js/Pages/Site/JobOffer/Show.vue
+++ resources/js/Pages/Site/JobOffer/Show.vue
@@ -1,10 +1,8 @@
 <script setup>
-import SiteLayout from "@/Layouts/SiteLayout.vue";
-import {Link, useForm} from '@inertiajs/inertia-vue3';
-import {computed} from "vue";
-import {isNull} from "lodash/lang";
-import {upperCase} from "lodash/string";
-import { ref } from 'vue'
+import { computed, ref} from "vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import { isNull } from "lodash/lang";
+import { upperCase } from "lodash/string";
 import {
     Dialog,
     DialogPanel,
@@ -12,13 +10,22 @@ import {
     DialogDescription,
 } from '@headlessui/vue'
 
+import SiteLayout from "@/Layouts/SiteLayout.vue";
+import Card from "@/Components/Card.vue";
+import Button from "@/Components/Button.vue";
+import Input from "@/Components/Form/Input.vue";
+import Textarea from "@/Components/Form/Textarea.vue";
+import InputError from "@/Components/Form/InputError.vue";
+
 const props = defineProps(['jobOffer'])
+
+const formatter = new Intl.NumberFormat('en-US')
 
 const salaries = computed(() => {
     return props.jobOffer.salary.map((salary) => {
         return {
             type: salary.type,
-            rate: ((salary.exact && !isNull(salary.exact)) ? salary.exact : `${salary.from} - ${salary.to}`) + ` ${upperCase(props.jobOffer.contract.currency)}`,
+            rate: ((salary.exact && !isNull(salary.exact)) ? formatter.format(salary.exact) : `${formatter.format(salary.from)} - ${formatter.format(salary.to)}`) + ` ${upperCase(props.jobOffer.contract.currency)}`,
             per: salary.per
         }
     })
@@ -41,22 +48,24 @@ const submit = () => form.post(route('site.jobs.apply', { jobOffer: props.jobOff
 </script>
 <template>
     <SiteLayout>
-        <div class="flex items-start gap-4 justify-between">
-            <div class="w-2/3 p-4 bg-neutral-100">
-                <div class="border-b border-neutral-200 pb-4">
-                    <div class="text-2xl font-bold">{{ jobOffer.title }}</div>
-                    <Link :href="route('site.companies.show', { company: jobOffer.company })">{{ jobOffer.company.name }}</Link>
+        <div class="my-8 flex justify-between items-start gap-4">
+            <Card class="w-2/3">
+                <div class="flex items-center gap-2 pb-2">
+                    <img :src="jobOffer.company.logo" :alt="jobOffer.company.name" class="rounded border object-cover h-12 w-12" />
+                    <div>
+                        <div class="text-2xl font-bold">{{ jobOffer.title }}</div>
+                        <div>{{ jobOffer.company.name }}</div>
+                    </div>
                 </div>
-                <div class="flex justify-between gap-4 py-2 border-b border-neutral-200 pb-2">
+                <div class="flex justify-between gap-4 py-4">
                     <div class="flex-1">
-                        <span>Category: </span>
-                        <template v-for="category in jobOffer.categories">
-                            <span class="font-bold mr-2">{{ category.name }}</span>
-                        </template>
+                        <span class="text-neutral-500">Category: </span>
+                        <span class="text-neutral-700 font-semibold">{{ jobOffer.categories.map((category) => category.name).join(", ") }}</span>
                     </div>
                     <div class="flex-1 flex items-center gap-2">
-                        <span class="capitalize">{{ jobOffer.level.name }}</span>
-                        <div class="flex items-center gap-1">
+                        <span class="text-neutral-500">Level: </span>
+                        <span class="capitalize text-neutral-700 font-semibold">{{ jobOffer.level.name }}</span>
+                        <div class="flex items-center gap-1 text-neutral-700">
                             <template v-for="n in jobOffer.level.value">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
                                     <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
@@ -66,59 +75,78 @@ const submit = () => form.post(route('site.jobs.apply', { jobOffer: props.jobOff
                     </div>
                 </div>
                 <div v-html="jobOffer.description" class="py-2"></div>
-            </div>
-            <div class="w-1/3 p-4 bg-neutral-100 flex flex-col gap-y-4">
-                <div class="divide-y divide-neutral-300">
+            </Card>
+            <Card class="w-1/3">
+                <div class="divide-y divide-neutral-200">
                     <template v-for="salary in salaries">
-                        <div class="py-2">
-                            <span class="uppercase">{{ salary.type }}</span>
+                        <div class="py-2 first:pt-0">
+                            <span class="uppercase text-sm text-neutral-500">{{ salary.type }}</span>
                             <div class="flex items-center gap-2">
-                                <span class="font-bold text-xl">{{ salary.rate }}</span>
-                                <span>/ {{ salary.per }}</span>
+                                <span class="font-semibold text-xl text-neutral-700">{{ salary.rate }}</span>
+                                <span class="text-sm text-neutral-500">/ {{ salary.per }}</span>
                             </div>
                         </div>
                     </template>
                 </div>
-                <button type="button" class="bg-neutral-900 text-neutral-50 py-6 text-lg" @click="setIsOpen(true)">Apply</button>
-                <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
-                    <div class="fixed inset-0 flex items-center justify-center">
-                        <DialogPanel class="w-full min-h-screen bg-white">
-                            <div class="p-4 flex justify-end">
-                                <button type="button" @click="setIsOpen(false)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div class="flex justify-center">
-                                <div class="w-1/3 flex flex-col">
-                                    <DialogTitle class="mb-4 text-center text-2xl font-bold">Apply for the job</DialogTitle>
-                                    <DialogDescription class="mb-8 p-2 border border-neutral-300">
-                                        {{ jobOffer.title }}
-                                        {{ jobOffer.salary[0] }}
-                                    </DialogDescription>
-                                    <form @submit.prevent="submit" class="flex flex-col gap-y-4">
-                                        <input type="text" v-model="form.name" placeholder="name and surname">
-                                        <input type="text" v-model="form.email" placeholder="email">
-                                        <textarea cols="30" rows="5" v-model="form.about" placeholder="about you and links"></textarea>
-                                        <input type="file" @change="form.cv = $event.target.files[0]">
-                                        <button type="submit" class="py-4 text-white bg-blue-600 hover:bg-blue-700">Apply</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </DialogPanel>
-                    </div>
-                </Dialog>
                 <div class="py-2">
-                    <div class="inline-flex gap-2">
+                    <Button type="button" @click="setIsOpen(true)" class="w-full">Apply</Button>
+                    <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+                        <div class="fixed inset-0 flex items-center justify-center">
+                            <DialogPanel class="w-full min-h-screen bg-white">
+                                <div class="p-4 flex justify-end">
+                                    <button type="button" @click="setIsOpen(false)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="flex justify-center">
+                                    <div class="w-1/3 flex flex-col">
+                                        <DialogTitle class="mb-4 text-center text-2xl font-bold">Apply for the job</DialogTitle>
+                                        <DialogDescription class="mb-8 p-2 bg-neutral-50 border border-neutral-300 rounded-lg">
+                                            <div class="flex items-center gap-2">
+                                                <img :src="jobOffer.company.logo" :alt="jobOffer.company.name" class="h-10 w-10 border border-neutral-200 rounded object-cover">
+                                                <div>
+                                                    <div class="font-medium text-neutral-700 transition-all hover:text-neutral-900">{{ jobOffer.title }}</div>
+                                                    <div class="block text-sm text-neutral-500">{{ jobOffer.company.name }}</div>
+                                                </div>
+                                            </div>
+                                        </DialogDescription>
+                                        <form @submit.prevent="submit" class="flex flex-col gap-y-4">
+                                            <div>
+                                                <Input type="text" v-model="form.name" placeholder="Name and surname" :error="form.errors.name"/>
+                                                <InputError :message="form.errors.name" />
+                                            </div>
+                                            <div>
+                                                <Input type="email" v-model="form.email" placeholder="Email" :error="form.errors.email"/>
+                                                <InputError :message="form.errors.email" />
+                                            </div>
+                                            <div>
+                                                <Textarea cols="30" rows="5" v-model="form.about" placeholder="About you and links" :error="form.errors.about" />
+                                                <InputError :message="form.errors.about" />
+                                            </div>
+                                            <div>
+                                                <input type="file" class="w-full block mb-1 px-2 py-1 text-neutral-700 border border-neutral-300 rounded-md shadow-sm" :class="[form.errors.cv ? 'border-red-400' : 'border-neutral-300']" @change="form.cv = $event.target.files[0]">
+                                                <InputError :message="form.errors.cv" />
+                                            </div>
+                                            <Button type="submit">Apply</Button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </DialogPanel>
+                        </div>
+                    </Dialog>
+                </div>
+                <div class="py-2">
+                    <div class="inline-flex gap-2 text-neutral-700">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                             <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
                             <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clip-rule="evenodd" />
                         </svg>
-                        <span>{{ jobOffer.expired_at }}</span>
+                        <span>Closing on {{ jobOffer.expired_at }}</span>
                     </div>
                 </div>
-            </div>
+            </Card>
         </div>
     </SiteLayout>
 </template>
---
+++ resources/js/Pages/Site/Partials/Job/JobsList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from '@/Components/Card.vue'
+import JobListItem from "@/Pages/Site/Partials/Job/JobListItem.vue";
+
+defineProps(['title', 'jobs'])
+</script>
+<template>
+    <Card>
+        <template #header>
+            <div class="text-neutral-900 font-semibold text-xl">{{ title }}</div>
+        </template>
+        <div class="divide-y divide-neutral-200">
+            <template v-for="job in jobs">
+                <JobListItem :job="job" />
+            </template>
+        </div>
+    </Card>
+</template>
