f6ddd018cf073f64753dc2cf9d4305ee9e8c3e9c
jkryszto00 <jakubkryszto@gmail.com>
2022-11-23 18:47:36 +0100

    company list and show view

--- resources/js/Pages/Site/Company/Index.vue
+++ resources/js/Pages/Site/Company/Index.vue
@@ -1,22 +1,30 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+
 import SiteLayout from "@/Layouts/SiteLayout.vue";
+import Button from "@/Components/Button.vue";
+import CompanyList from "@/Pages/Site/Partials/Company/CompanyList.vue";
 
 defineProps(['companies', 'companiesCount'])
 </script>
 
 <template>
     <SiteLayout>
-        <div class="mb-8">
-            <div class="mb-2 font-bold text-2xl">Companies ({{ companiesCount }})</div>
-            <div class="bg-neutral-50 border border-neutral-300 divide-y divide-neutral-300">
-                <template v-for="company in companies">
-                    <div class="p-4 flex justify-between items-center hover:bg-neutral-200">
-                        <Link :href="route('site.companies.show', { company: company })" class="text-xl">{{ company.name }}</Link>
-                        <span>Jobs ({{ company.jobOfferCount }})</span>
-                    </div>
-                </template>
-            </div>
+        <CompanyList
+            class="my-8"
+            :title="`Companies (${companiesCount})`"
+            :companies="companies"
+        />
+
+        <div class="mb-8 flex justify-center">
+            <Link :href="route('site.jobs.index')">
+                <Button class="flex items-center gap-2">
+                    More companies
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path fill-rule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clip-rule="evenodd" />
+                    </svg>
+                </Button>
+            </Link>
         </div>
     </SiteLayout>
 </template>
