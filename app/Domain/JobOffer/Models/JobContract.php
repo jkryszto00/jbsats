13c99142abf5c02f083cdc6ee9ce2341bf43c1b7
jkryszto00 <jakubkryszto@gmail.com>
2022-11-28 02:42:21 +0100

    return types

--- app/Domain/JobOffer/Models/JobContract.php
+++ app/Domain/JobOffer/Models/JobContract.php
@@ -4,6 +4,7 @@ namespace App\Domain\JobOffer\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobContract extends Model
 {
@@ -13,7 +14,7 @@ class JobContract extends Model
 
     public $timestamps = false;
 
-    public function jobOffer()
+    public function jobOffer(): BelongsTo
     {
         return $this->belongsTo(JobOffer::class);
     }
